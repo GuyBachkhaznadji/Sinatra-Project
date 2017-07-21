4ff6f3014292ea4853becbb5e6d6933f7bd2a3d0
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-21 12:04:42 +0100

    added a couple of columns in my sql table

--- db/arena.sql
+++ db/arena.sql
@@ -4,8 +4,9 @@ DROP TABLE IF EXISTS fighters;
 CREATE TABLE creatures(
 id SERIAL4 PRIMARY KEY,
 name VARCHAR(255),
+caputure_date DATE,
 fightable BOOLEAN,
-fighter_id INT4
+fighter_id INT4 REFERENCES fighters(id)
 );
 
 CREATE TABLE fighters(
