1079f95d3b4a87d916b86205ca18bb7dc991f43f
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-22 19:12:24 +0100

    added creature destory routes

--- controllers/creatures_controller.rb
+++ controllers/creatures_controller.rb
@@ -37,3 +37,17 @@ post '/creatures/:id' do
   creature.update
   redirect '/creatures'
 end
+
+#GET DESTROY
+get '/creatures/:id/destroy' do
+  @creatures = Creature.all
+  @id = params[:id]
+  erb(:"creatures/destroy")
+end
+
+#DESTROY
+post '/creatures/:id/destroy' do
+  creature = Creature.find(params[:id])
+  creature.delete
+  redirect '/creatures'
+end
