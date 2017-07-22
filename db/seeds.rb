dbdc15cc750f460a44439860c1517b830af27b88
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-22 21:04:57 +0100

    updated seeds file to reflect new creature category

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,16 @@ gladiator1.save
 gladiator2 = Gladiator.new({'name' => "Atlas Nero"})
 gladiator2.save
 
-creature1 = Creature.new({'name' => "Nembit, The Firestarter", 'capture_date' => "5/5/0100", 'fightable' => 't', 'gladiator_id' => gladiator1.id})
+creature1 = Creature.new({'name' => "Nembit, The Firestarter", 'type' => "Dragon", 'capture_date' => "5/5/0100", 'fightable' => 't', 'gladiator_id' => gladiator1.id})
 creature1.save
 
-creature2 = Creature.new({'name' => "Smaug, The Terrible", 'capture_date' => "15/10/0500", 'fightable' => 'f', 'gladiator_id' => gladiator2.id})
+creature2 = Creature.new({'name' => "Smaug, The Terrible", 'type' => "Dragon", 'capture_date' => "15/10/0500", 'fightable' => 'f', 'gladiator_id' => gladiator2.id})
 creature2.save
 
-creature3 = Creature.new({'name' => "Leo, The Loud", 'capture_date' => "15/10/0235", 'fightable' => 'f'})
+creature3 = Creature.new({'name' => "Leo, The Loud", 'type' => "Lion", 'capture_date' => "15/10/0235", 'fightable' => 'f'})
 creature3.save
 
-creature4 = Creature.new({'name' => "Ursa, The Tall", 'capture_date' => "15/10/0235", 'fightable' => 'f', 'gladiator_id' => gladiator2.id})
+creature4 = Creature.new({'name' => "Ursa, The Tall",'type' => "Bear", 'capture_date' => "15/10/0235", 'fightable' => 'f', 'gladiator_id' => gladiator2.id})
 creature4.save
 
 gladiator3 = Gladiator.new({'name' => "Anthony Digitius"})
