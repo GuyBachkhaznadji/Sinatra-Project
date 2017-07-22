b352303f4a057f9c672379ed2392123f1465ccfe
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-22 18:25:24 +0100

    added an update function to gladiators

--- models/gladiator.rb
+++ models/gladiator.rb
@@ -20,6 +20,15 @@ class Gladiator
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE gladiators
+    SET 
+    name = $1, 
+    WHERE id = $2;"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def creatures()
     sql = "SELECT * FROM creatures
     WHERE $1 = gladiator_id;"
