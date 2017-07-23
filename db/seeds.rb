b40fb6aa0c16fe2dc68b2e780f79866d510e4e78
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-23 17:23:14 +0100

    added a font

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,6 @@ gladiator1.save
 gladiator2 = Gladiator.new({'name' => "Atlas Nero"})
 gladiator2.save
 
-creature1 = Creature.new({'name' => "Nembit, The Firestarter", 'type' => "Dragon", 'capture_date' => "5/5/0100", 'fightable' => 't', 'gladiator_id' => gladiator1.id})
-creature1.save
-creature1.update
-
 creature2 = Creature.new({'name' => "Smaug, The Terrible", 'type' => "Dragon", 'capture_date' => "15/10/0500", 'fightable' => 'f'})
 creature2.save
 creature2.update
@@ -21,6 +17,10 @@ creature2.update
 creature3 = Creature.new({'name' => "Leo, The Loud", 'type' => "Lion", 'capture_date' => "15/10/0235", 'fightable' => 'f'})
 creature3.save
 creature3.update
+
+creature1 = Creature.new({'name' => "Nembit, The Firestarter", 'type' => "Dragon", 'capture_date' => "5/5/0100", 'fightable' => 't', 'gladiator_id' => gladiator1.id})
+creature1.save
+creature1.update
 
 creature4 = Creature.new({'name' => "Ursa, The Tall",'type' => "Bear", 'capture_date' => "15/10/0235", 'fightable' => 'f'})
 creature4.save
