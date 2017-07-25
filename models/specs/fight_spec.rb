fe745481435a6d8e9f3aa47d6c6c2b24539c844e
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-25 10:15:44 +0100

    re ordered tests to make them logical progression

--- models/specs/fight_spec.rb
+++ models/specs/fight_spec.rb
@@ -17,13 +17,11 @@ class TestFight < MiniTest::Test
     @creature1 = Creature.new({'name' => "Nembit, The Firestarter", 'type' => "Dragon", 'capture_date' => "5/5/0100", 'fightable' => 't', 'gladiator_id' => gladiator1.id, 'level' => 1, "exp" => 0})
     @creature1.save
     @creature1.update
-
-
   end
-
-  def test_level_up?
-    fighter = gladiator1.level_up?
-    assert_equal(true, fighter)
+  
+  def test_first
+    fighter = Fight.first(gladiator1, creature1)
+    assert_equal(creature1, fighter)
   end
 
   def test_win?
@@ -31,19 +29,19 @@ class TestFight < MiniTest::Test
     assert_equal(true, fighter)
   end
 
-  def test_level_up
-    gladiator1.level_up
-    assert_equal(2, gladiator1.level)
-  end
-
   def test_exp_up
     gladiator1.exp_up
     assert_equal(1, gladiator1.exp)
   end
 
-  def test_first
-    fighter = Fight.first(gladiator1, creature1)
-    assert_equal(creature1, fighter)
+  def test_level_up?
+    fighter = gladiator1.level_up?
+    assert_equal(true, fighter)
+  end
+
+  def test_level_up
+    gladiator1.level_up
+    assert_equal(2, gladiator1.level)
   end
 
   # def test_attack
